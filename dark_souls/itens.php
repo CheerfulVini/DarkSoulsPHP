26a3eae17de079c2b4b8a0d59b2a13995e0494b0
Vinícius Brevesteky Teixeira <130590509+CheerfulVini@users.noreply.github.com>
2024-11-21 12:14:36 -0300

    Delete dark_souls directory

--- dark_souls/itens.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once("model/Item.php");
-
-$espada = new Item("Espada", 1, 50, 3, 6, 'a');
-$cajado_simples = new CajadoSimples("Cajado Simples", 1, 50, 2, 6, 'a');
-$adaga = new Item("Adaga", 1, 50, 1.5, 6, 'a');
--- dark_souls/model/IBatalha.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-interface Batalha{
-    public function atacar($distancia, $dano_bonus, $bonus_item, $vida, $CA);
-    public function recuar($distancia);
-    public function avancar($distancia);
-}
